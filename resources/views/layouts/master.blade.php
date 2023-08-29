ff05ca5ce2e0321f7d1fa819fdd917bd81101402
coke <coke@gmail.com>
2023-08-29 13:34:42 +0700

    commit plugin

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,13 +9,14 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Blank</title>
+    <title>@yield('titile')</title>
 
     <!-- Custom fonts for this template-->
-    <link href="{{asset ('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{asset ('assets/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
+    <link media="all" type="text/css" rel="stylesheet" href="{{ url('assets/plugins/dataTables/datatables.min.css') }}">
 
     <!-- Custom styles for this template-->
     <link href="{{asset ('assets/css/sb-admin-2.min.css')}}" rel="stylesheet">
@@ -35,7 +36,7 @@
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+                <div class="sidebar-brand-text mx-3">Mutelu Admin<sup>2</sup></div>
             </a>
 
             <!-- Divider -->
@@ -57,20 +58,7 @@
             </div>
 
             <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
-                    aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Components</span>
-                </a>
-                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
-                        <h6 class="collapse-header">Custom Components:</h6>
-                        <a class="collapse-item" href="buttons.html">Buttons</a>
-                        <a class="collapse-item" href="cards.html">Cards</a>
-                    </div>
-                </div>
-            </li>
+            @include('partials.navbar')
 
             <!-- Nav Item - Utilities Collapse Menu -->
             <li class="nav-item">
@@ -364,7 +352,7 @@
                 <div class="container-fluid">
 
                     <!-- Page Heading -->
-                    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+                    @yield('content')
 
                 </div>
                 <!-- /.container-fluid -->
@@ -413,15 +401,18 @@
         </div>
     </div>
 
+    
     <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
+    <script src="{{asset ('assets/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset ('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    {{-- include Js --}}
+    <script src="{{url ('assets/plugins/dataTables/datatables.min.js') }}"></script>
     <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+    <script src="{{asset ('assets/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
 
     <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+    <script src="{{asset ('assets/js/sb-admin-2.min.js')}}"></script>
+    @yield('scripts')
 
 </body>
 
---
+++ resources/views/posts/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('titile', 'AdminControl')
+
+@section('content')
+<div class="card">
+    <div class="card-header">
+        <h1>โพสต์</h1>
+    </div>
+    <div class="card-body">
+        
+    </div>
+</div>
+@endsection
